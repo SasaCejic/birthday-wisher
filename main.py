187f53c4aef0ff0d6795a4f300a2f52425f638bd
SasaCejic <cejicsasa17@gmail.com>
2025-09-03 16:52:08 +0200

    Implemented files reading and email sending logic and added README file

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-##################### Extra Hard Starting Project ######################
+import datetime as dt
+import pandas
+import smtplib
+import random
 
-# 1. Update the birthdays.csv
+USER = "cejicsasa17@gmail.com"
+PASSWORD = "YOUR_APP_PASSWORD"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+def send_greeting_card(birthday):
+    file_number = random.randint(1,3)
+    with open(file=f"letter_templates/letter_{file_number}.txt", mode="r") as file:
+        letter = file.read().replace("[NAME]", birthday["name"])
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=USER, password=PASSWORD)
+        connection.sendmail(
+            from_addr=USER,
+            to_addrs=birthday["email"],
+            msg="Subject:Happy Birthday\n\n"
+                f"{letter}"
+        )
 
-# 4. Send the letter generated in step 3 to that person's email address.
+df = pandas.read_csv("birthdays.csv")
+birthdays_dict = df.to_dict(orient="records")
 
+today = dt.datetime.now()
 
-
-
+for birthday in birthdays_dict:
+    if birthday["month"] == today.month and birthday["day"] == today.day:
+        send_greeting_card(birthday)
